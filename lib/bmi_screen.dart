f981b0617440ddbef75e11164c4b3c3f1016e855
saci zakaria <sacideveloppeur@gmail.com>
2021-12-17 09:41:47 +0100

    Scaffold/appBar/body/Center

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class BmiScreen extends StatefulWidget {
@@ -10,6 +14,9 @@ class BmiScreen extends StatefulWidget {
 class _BmiScreenState extends State<BmiScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("BMI Calculator")),
+      body: Center(child: Text('BMI Calculator')),
+    );
   }
 }
