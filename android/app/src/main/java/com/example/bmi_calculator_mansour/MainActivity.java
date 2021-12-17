a9893633ceb766f20a40f6aad8b44a29a25758e3
saci zakaria <sacideveloppeur@gmail.com>
2021-12-17 09:35:45 +0100

    BmiScreen

---
+++ android/app/src/main/java/com/example/bmi_calculator_mansour/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.bmi_calculator_mansour;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/bmi_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BmiScreen extends StatefulWidget {
+  const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  _BmiScreenState createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
