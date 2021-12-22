29db8f2117130c6bea6e88eab20e763e905b24e2
saci zakaria <sacideveloppeur@gmail.com>
2021-12-22 15:08:33 +0100

    AppBar

--- lib/bmi_result_screen.dart
+++ lib/bmi_result_screen.dart
@@ -5,6 +5,11 @@ class BmiResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Result Screen'),
+        centerTitle: true,
+      ),
+    );
   }
 }
